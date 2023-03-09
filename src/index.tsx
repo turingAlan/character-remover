61431205b1b4e56962f13d508970e4a5bb115f3f
Sarthak Jain <sarthak2107597@st.jmi.ac.in>
2023-03-09 22:21:07 +0530

    add config for gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 
+import { HashRouter as Router } from "react-router-dom";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
